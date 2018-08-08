87384b3fb20ec99e7615a5bef28f1301acc36918
chriskilinc <chriskilinc@gmail.com>
2018-08-08 21:51:08 +0200

    Added About Component

--- src/components/About.js
+++ src/components/About.js
@@ -3,43 +3,52 @@ import './About.css';
 
 class About extends Component {
   render() {
-    return (
-      this.props.navState != "fixed-nav" ?
-        <section id="about" className="about">
-          <div className="about--container">
-            <h1>Hello!</h1>
-            <p>
-              I am a social and outgoing person with a great interest in technology and how things work.
-            </p>
-            <p>
-              I want to surround myself with focused and inspirational people who burn for what they do.
-            </p>
-            <p>
-              I have recently registered a private company, Drevit. I build and sell specialized web applications.
-            </p>
-            <p>
-              My time in addition to running the company, I play music, learn Game Development as well as experimenting with IOT solutions such as Raspberry Pi, z-wave and OpenHab.
-            </p>
-          </div>
-        </section>
-        :
-        <section id="about" className="about spacer">
-          <div className="about--container">
-            <h1>Hello!</h1>
-            <p>
-              I am a social and outgoing person with a great interest in technology and how things work.
-            </p>
-            <p>
-              I want to surround myself with focused and inspirational people who burn for what they do.
-            </p>
-            <p>
-              I have recently registered a private company, Drevit. I build and sell specialized web applications.
-            </p>
-            <p>
-              My time in addition to running the company, I play music, learn Game Development as well as experimenting with IOT solutions such as Raspberry Pi, z-wave and OpenHab.
-            </p>
-          </div>
-        </section>
+    return this.props.navState !== 'fixed-nav' ? (
+      <section id="about" className="about">
+        <div className="about--container">
+          <h1>Hello!</h1>
+          <p>
+            I am a social and outgoing person with a great interest in
+            technology and how things work.
+          </p>
+          <p>
+            I want to surround myself with focused and inspirational people who
+            burn for what they do.
+          </p>
+          <p>
+            I have recently registered a private company, Drevit. I build and
+            sell specialized web applications.
+          </p>
+          <p>
+            My time in addition to running the company, I play music, learn Game
+            Development as well as experimenting with IOT solutions such as
+            Raspberry Pi, z-wave and OpenHab.
+          </p>
+        </div>
+      </section>
+    ) : (
+      <section id="about" className="about spacer">
+        <div className="about--container">
+          <h1>Hello!</h1>
+          <p>
+            I am a social and outgoing person with a great interest in
+            technology and how things work.
+          </p>
+          <p>
+            I want to surround myself with focused and inspirational people who
+            burn for what they do.
+          </p>
+          <p>
+            I have recently registered a private company, Drevit. I build and
+            sell specialized web applications.
+          </p>
+          <p>
+            My time in addition to running the company, I play music, learn Game
+            Development as well as experimenting with IOT solutions such as
+            Raspberry Pi, z-wave and OpenHab.
+          </p>
+        </div>
+      </section>
     );
   }
 }
