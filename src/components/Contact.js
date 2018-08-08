1c193a4f192f6045596dfa92f567ad631e067210
chriskilinc <chriskilinc@gmail.com>
2018-08-08 21:52:41 +0200

    Added Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,33 +6,44 @@ class Contact extends Component {
     return (
       <section id="contact" className="contact">
         <div className="contact--container">
-
           <div className="contact--items">
-
             <div className="contact--item">
               <div className="contact--item--container">
-                <a href="https://github.com/chriskilinc" target="_blank"><i class="fab fa-github"></i></a>
+                <a
+                  href="https://github.com/chriskilinc"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Open Website Github"
+                  title="Porfolio over at github.com"
+                >
+                  <i class="fab fa-github" />
+                </a>
                 <p>Github</p>
               </div>
             </div>
 
             <div className="contact--item">
               <div className="contact--item--container">
-                <a href="https://www.linkedin.com/in/chriskilinc/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                <a
+                  href="https://www.linkedin.com/in/chriskilinc/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Open Website Linkedin"
+                  title="Porfolio over at linkedin.com"
+                >
+                  <i class="fab fa-linkedin" />
+                </a>
                 <p>Linkedin</p>
               </div>
             </div>
 
             <div className="contact--item">
               <div className="contact--item--container">
-                <a href="" target="_blank"></a>
                 <p>+46 736 647 293</p>
                 <p>chriskilinc@gmail.com</p>
               </div>
             </div>
-
           </div>
-
         </div>
       </section>
     );
