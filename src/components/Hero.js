c9706d553a549c1ca5423e35b3ec84f1156f140c
chriskilinc <chriskilinc@gmail.com>
2018-08-08 21:50:34 +0200

    Added Hero Component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-
 class Hero extends Component {
   render() {
     return (
-      
-        <section className="hero">
-          <div className="hero--icon-container">
-            <img className="icon" src={this.props.icon} alt="Selfie Circle" />
-          </div>
-          <div className="hero--content">
-            <h1>chriskilinc</h1>
-            <div className="flex-break-row"></div>
-            <p className="quote">Aspiring Software Developer</p>
-          </div>
-        </section>
-        
+      <section className="hero">
+        <div className="hero--icon-container">
+          <img className="icon" src={this.props.icon} alt="Selfie Circle" />
+        </div>
+        <div className="hero--content">
+          <h1>chriskilinc</h1>
+          <div className="flex-break-row" />
+          <p className="quote">Aspiring Software Developer</p>
+        </div>
+      </section>
     );
   }
 }
