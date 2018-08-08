50eccdd17d91849234131fd91d79af2c374aa7e4
chriskilinc <chriskilinc@gmail.com>
2018-08-08 21:52:17 +0200

    Added Skills Component to Expertise Component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,7 +10,9 @@ class Skills extends Component {
             <h1 className="title">Expertise</h1>
             <div className="skills--row">
               <div className="skills--col">
-                <h2><i className="fas fa-code"></i>JavaScript</h2>
+                <h2>
+                  <i className="fas fa-code" />JavaScript
+                </h2>
                 <div className="skills--col--content">
                   <p>ReactJs</p>
                   <p>NodeJs</p>
@@ -18,7 +20,9 @@ class Skills extends Component {
                 </div>
               </div>
               <div className="skills--col">
-                <h2><i className="fab fa-windows"></i>.NET</h2>
+                <h2>
+                  <i className="fab fa-windows" />.NET
+                </h2>
                 <div className="skills--col--content">
                   <p>C#</p>
                   <p>ASP.NET/ MVC / WPF</p>
@@ -26,15 +30,18 @@ class Skills extends Component {
                 </div>
               </div>
               <div className="skills--col">
-                <h2><i className="fab fa-html5"></i>HTML / CSS</h2>
+                <h2>
+                  <i className="fab fa-html5" />HTML / CSS
+                </h2>
                 <div className="skills--col--content">
                   <p>HTML5</p>
                   <p>CSS / SCSS ( BEM )</p>
-
                 </div>
               </div>
               <div className="skills--col">
-                <h2><i className="fas fa-database"></i>Databases</h2>
+                <h2>
+                  <i className="fas fa-database" />Databases
+                </h2>
                 <div className="skills--col--content">
                   <p>MSSQL</p>
                   <p>MongoDB</p>
@@ -42,7 +49,9 @@ class Skills extends Component {
                 </div>
               </div>
               <div className="skills--col">
-                <h2><i className="fas fa-angle-right"></i>Other</h2>
+                <h2>
+                  <i className="fas fa-angle-right" />Other
+                </h2>
                 <div className="skills--col--content">
                   <p>Git</p>
                   <p>WordPress</p>
@@ -51,7 +60,9 @@ class Skills extends Component {
                 </div>
               </div>
               <div className="skills--col">
-                <h2><i className="fas fa-desktop"></i>Software</h2>
+                <h2>
+                  <i className="fas fa-desktop" />Software
+                </h2>
                 <div className="skills--col--content">
                   <p>Visual Studio / Code</p>
                   <p>Unity Engine</p>
@@ -60,8 +71,6 @@ class Skills extends Component {
                   <p>Fruity Loops Studio</p>
                 </div>
               </div>
-
-
             </div>
           </div>
         </div>
