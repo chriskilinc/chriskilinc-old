cd8671e91fa61ffc73cbd436917a705d0217b7c6
chriskilinc <chriskilinc@gmail.com>
2018-08-08 21:50:03 +0200

    Added Navigation Component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,25 +4,37 @@ import NavigationItem from './NavigationItem';
 
 // https://github.com/joshwnj/react-visibility-sensor
 class Navigation extends Component {
-
   render() {
-    return (
-      this.props.navState == "normal-nav" ?
-        <nav className="navigation" ref="nav">
-          <div className="navigation--container">
-            {/* <NavigationItem isActive={true} id={"about"} changeActive={this.handleOnNavItemClick}/>
+    return this.props.navState === 'normal-nav' ? (
+      <nav className="navigation" ref="nav">
+        <div className="navigation--container">
+          {/* <NavigationItem isActive={true} id={"about"} changeActive={this.handleOnNavItemClick}/>
           <NavigationItem isActive={false} id={"experties"} changeActive={this.handleOnNavItemClick}/>
           <NavigationItem isActive={false} id={"contact"} changeActive={this.handleOnNavItemClick} /> */}
-            {this.props.navItems.map(item => <NavigationItem key={item.id} isActive={item.isActive} id={item.id} changeToActive={this.props.changeToActive} />)}
-          </div>
-        </nav>
-        :
-        <nav className="navigation fixed-nav" ref="nav">
-          <div className="navigation--container">
+          {this.props.navItems.map(item => (
+            <NavigationItem
+              key={item.id}
+              isActive={item.isActive}
+              id={item.id}
+              changeToActive={this.props.changeToActive}
+            />
+          ))}
+        </div>
+      </nav>
+    ) : (
+      <nav className="navigation fixed-nav" ref="nav">
+        <div className="navigation--container">
           <p>/ Chriskilinc /</p>
-            {this.props.navItems.map(item => <NavigationItem key={item.id} isActive={item.isActive} id={item.id} changeToActive={this.props.changeToActive} />)}
-          </div>
-        </nav>
+          {this.props.navItems.map(item => (
+            <NavigationItem
+              key={item.id}
+              isActive={item.isActive}
+              id={item.id}
+              changeToActive={this.props.changeToActive}
+            />
+          ))}
+        </div>
+      </nav>
     );
   }
 }
