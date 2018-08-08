9c79d9491ec7005db48ec39f63fd8b7082f52822
chriskilinc <chriskilinc@gmail.com>
2018-08-08 21:52:07 +0200

    Added Education Component to Expertise Component

--- src/components/Education.js
+++ src/components/Education.js
@@ -10,29 +10,27 @@ class Education extends Component {
             <h1 className="title">Education</h1>
 
             <div className="education--items">
-
               <div className="education--item">
-                {/* <h2>Östra Gymnasiet<span>Technical College Graduate</span></h2> */}
                 <div className="education--item--titles">
                   <h2>Technical College Graduate</h2>
                   <h3>2015-2016</h3>
                 </div>
                 <div className="education--item--content">
                   <h3>Östra Gymnasiet</h3>
-                  {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed ornare nulla. Aenean molestie tristique libero quis dapibus. Cras gravida vel tellus a feugiat. Fusce non consequat nisi. Quisque pellentesque arcu ut dolor aliquam, eu semper massa dignissim. Sed lacus orci, efficitur non viverra non, lacinia eget erat. In in egestas dolor.</p> */}
-                  {/* <h4>Courses</h4>
-                      <ul>
-                        <li> Data Storage</li>
-                        <li> Interface Design</li>
-                        <li> Engineer in practice</li>
-                        <li> Software Design</li>
-                        <li> Computer Science</li>
-                        <li> Programming 2</li>
-                        <li> Technology - Specialization</li>
-                      </ul> */}
+
                   <h4>Master Thesis Report</h4>
-                  <p>(SMS Gateway Admin - Development of a web platform for existing systems in administrative purposes)</p>
-                  <a href="http://www.ostragymnasiet.se/index.php?sida=13&id=730" target="_blank">Course/Program Website</a>
+                  <p>
+                    (SMS Gateway Admin - Development of a web platform for
+                    existing systems in administrative purposes)
+                  </p>
+                  <a
+                    href="http://www.ostragymnasiet.se/index.php?sida=13&id=730"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Information about education program"
+                  >
+                    Course/Program Website
+                  </a>
                 </div>
               </div>
 
@@ -45,12 +43,20 @@ class Education extends Component {
                   <h3>Östra Gymnasiet</h3>
                   <p>Information- and Media Technology 2500p</p>
                   <h4>Gymnasial Thesis Report</h4>
-                  <p>Computer Game created in Unity game engine and written in c#</p>
-                  <a href="http://www.ostragymnasiet.se/index.php?sida=13&id=476" target="_blank">Course/Program Website</a>
+                  <p>
+                    Computer Game created in Unity game engine and written in c#
+                  </p>
+                  <a
+                    href="http://www.ostragymnasiet.se/index.php?sida=13&id=476"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Information about education program"
+                  >
+                    Course/Program Website
+                  </a>
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
